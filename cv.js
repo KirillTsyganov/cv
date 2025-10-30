bb1b057c1959f8e71fc21fa41025ef193bc38e23
Kirill Tsyganov <kirill.tsyganov@outlook.com.au>
2025-10-30 23:40:33 +1100

    using static file

--- cv.js
+++ cv.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
         return;
     }
 
-    fetch('cvs/KirillTsyganovCV.md')
+    fetch('cv.md')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Network response was not ok: ${response.statusText}`);
